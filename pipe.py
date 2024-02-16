caf439ee61a69a3fa0c87a7bf5249c4657c140a0
IStoleYourCookie <szekely.mkls@gmail.com>
2024-02-16 19:50:39 +0100

    Added clearing screen and some additional fluid logic and fixed a few bugs

--- pipe.py
+++ pipe.py
@@ -1,4 +1,11 @@
 import time
+from os import system, name
+
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
 
 print("Hello World!")
 
@@ -10,9 +17,9 @@ class pipe:
         self.position = position
         self.connections = connections
 
-pipes = [pipe(60, 60, 1, 3, [2, 1]),
-         pipe(60, 0, -0.5, 2, [0]),
-         pipe(60, 0, -0.5, 1, [0])]
+pipes = [pipe(60, 60, 1, 3, [1]),
+         pipe(10, 0, 1, 2, [2]),
+         pipe(10, 0, 0, 1, [0])]
 
 loop = True
 toc = 0
@@ -28,22 +35,34 @@ while loop:
 
     for p in pipes:
         if p.fluid > 0:
-            p.fluid -= p.flowrate * len(p.connections) * dtime
+            valid_cons = 0
+
+            for d in p.connections:
+                if pipes[d].fluid != pipes[d].volume:
+                    valid_cons += 1
+
+            p.fluid -= p.flowrate * valid_cons * dtime
+
         if p.fluid > p.volume:
             p.fluid = p.volume
         if p.fluid < 0:
             p.fluid = 0
 
         for c in p.connections:
+
             if pipes[c].position < p.position:
+                
                 pipes[c].fluid += p.flowrate * dtime
+
                 if pipes[c].fluid > pipes[c].volume:
                     pipes[c].fluid = pipes[c].volume
                 if pipes[c].fluid < 0:
                     pipes[c].fluid = 0
 
     toc = time.perf_counter()
+    clear()
     print(f"tic: {tic:0.4f} | toc: {toc:0.4f} | dtime: {dtime:0.4f}")
     for p in pipes:
         print(f"volume: {p.volume} | fluid: {(p.fluid):0.4f} | flowrate: {p.flowrate} | position: {p.position} | connections: {p.connections}")
-
+    print("")
+    #command = input("New command: ")
